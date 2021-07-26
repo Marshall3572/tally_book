3ee1d28400e7d09b6c1f853045644fbe7c6e7432
Marshall3572 <944797458@qq.com>
2021-07-26 21:44:24 +0800

    搞定3个icon

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,6 +1,9 @@
 import {Link} from 'react-router-dom';
 import React from 'react';
 import styled from 'styled-components';
+require('../icons/money.svg');  // svgo-loader => svg-sprite-loader这个loader会把我们引入的svg标签在body里作为一个symbol，合并成一个大的svg
+require('../icons/tag.svg');
+require('../icons/chart.svg');
 
 const Nav = styled.nav`
   line-height: 24px;
@@ -12,7 +15,15 @@ const Nav = styled.nav`
     li {
       width: 33.333333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      //justify-content: center;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -22,13 +33,22 @@ export default () => {
     <Nav>
       <ul>
         <li>
-          <img />
+          <svg className="icon">
+            {/* 可以在控制台打出，看得到id*/}
+            <use xlinkHref="#tag"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
