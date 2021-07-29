9589426c083d2163d4e770f9802ad6040f9c38a9
Marshall3572 <944797458@qq.com>
2021-07-29 11:26:43 +0800

    记账页骨架完成，待添加样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,66 @@
 import Layout from '../component/layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+
+`
+
+const NotesSection = styled.section`
+
+`
+const CategorySection = styled.section`
+
+`
+
+const PadSection = styled.section`
+
+`
+
 
 const Money = () => {
   return (
     <Layout>
-      <h2>记账页面</h2>
+      <TagsSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>新增标签</button>
+      </TagsSection>
+      <NotesSection>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </NotesSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <PadSection>
+        <div>数值</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </PadSection>
     </Layout>
   );
 };
