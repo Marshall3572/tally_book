beea7d39f67b67eadf0c7a95ed89e4348ef3ff11
Marshall3572 <944797458@qq.com>
2021-07-30 07:03:43 +0800

    Money.tsx之支出收入类型UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -50,6 +50,29 @@ const NotesSection = styled.section`
   }
 `;
 const CategorySection = styled.section`
+  font-size: 24px;
+  > ul{
+    display: flex;
+    background: #c4c4c4;
+    > li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      //为了让下划线border不占高度，使用伪元素
+      &.selected::after{
+        content: '';
+        background: #333;
+        // 默认另起一行
+        display: block;
+        height: 3px;
+        position: absolute;
+        width: 100%;
+        left: 0;
+        bottom: 0;
+      }
+    }
+  }
 `;
 const NumberPadSection = styled.section`
 `;
@@ -75,7 +98,7 @@ const Money = () => {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
