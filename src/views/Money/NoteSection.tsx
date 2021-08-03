43f23ea3483a239467c43ce595bb9b1f2f5ab8ac
Marshall3572 <944797458@qq.com>
2021-08-03 14:53:41 +0800

    React和HTML的onChange触发时刻对比

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -35,6 +35,8 @@ const NoteSection: FC = () => {
         {/*<input type="text" placeholder="在这里添加备注" value={note} onChange={e => {setNote(e.target.value);}}/>*/}
         {/*为了巩固新学的知识，加入非受控模式，鼠标移出输入框触发*/}
         <input type="text" placeholder="在这里添加备注" defaultValue={note} ref={refInput} onBlur={onBlur}/>
+        {/*这里补充一点，React中的onChange和html中的onchange(比如addEventListener)是不一样的*/}
+        {/*React的onChange在你每输入一个字触发一次。而html的onChange在鼠标移出时触发(早于onBlur)*/}
       </label>
     </Wrapper>
   );
