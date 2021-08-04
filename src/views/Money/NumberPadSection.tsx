2514df1ef122d8cbd299cc8279c2d6c3f61a6ab8
Marshall3572 <944797458@qq.com>
2021-08-04 16:18:31 +0800

    重构 NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,74 +1,6 @@
-import styled from 'styled-components';
 import React, {FC, useState, MouseEvent} from 'react';
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  > .output {
-    background: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25) inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-
-  > .pad {
-    > button {
-      font-size: 18px;
-      //这里不能使用flex布局，因为"ok"很高，把"0 ."挤下去了
-      float: left;
-      width: 25%;
-      height: 64px;
-      border: none;
-
-      &.ok {
-        height: 128px;
-        float: right;
-      }
-
-      &.zero {
-        width: 50%;
-      }
-
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #E0E0E0;
-      }
-
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #D3D3D3;
-      }
-
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #C1C1C1;
-      }
-
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #B8B8B8;
-      }
-
-      &:nth-child(12) {
-        background: #9A9A9A;
-      }
-
-      &:nth-child(14) {
-        background: #A9A9A9;
-      }
-    }
-  }
-`;
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
 const NumberPadSection: FC = () => {
   const [output, _setOutput] = useState<string>('0');
@@ -78,41 +10,14 @@ const NumberPadSection: FC = () => {
     _setOutput(output)
   };
   const onClickButtonWrapper = (e: MouseEvent) => {
-    const text: string | null = (e.target as HTMLButtonElement).textContent;
+    const text = (e.target as HTMLButtonElement).textContent;
     if (!text) return;
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) return;
-        setOutput(output + '.');
-        break;
-      case '删除':
-        if (output.length === 1) {setOutput('');} else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
+    if (text === 'OK') {
+      // TODO
+      return;
     }
+    if('0123456789.'.split('').concat(['删除','清空']).indexOf(text) >= 0)
+    setOutput(generateOutput(text, output))
   };
   return (
     <Wrapper>
